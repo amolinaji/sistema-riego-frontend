5ef0b769585cfe4a35bdf884d28fec5224947e58
Freddy fullstack <puertafreddy24@gmail.com>
2025-03-04 14:58:13 -0500

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
-const isGithubPages = process.env.GITHUB_ACTIONS || false;
-const repoName = "sistema-riego-frontend"; // Nombre de tu repositorio
+import type { NextConfig } from "next";
 
-const nextConfig = {
-  output: "export", // Necesario para GitHub Pages
-  basePath: isGithubPages ? `/${repoName}` : "", // Ajuste del path
-  images: {
-    unoptimized: true, // Deshabilita la optimización de imágenes en GitHub Pages
-  },
-  env: {
-    NEXT_PUBLIC_API_URL:
-      process.env.NEXT_PUBLIC_API_URL ||
-      "https://sistema-riego-api-production.up.railway.app",
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
